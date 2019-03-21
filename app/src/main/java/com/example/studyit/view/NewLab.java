c3c09f07660134ba949112ae3f82bad93efa6b09
m.zolotarev <m.zolotarev@xsolla.com>
2019-03-21 19:31:06 +0500

    добавили возмжность ручного добавления новой лабы
    Епанов Андрей
    Золотарев Максим

--- app/src/main/java/com/example/studyit/view/NewLab.java
+++ app/src/main/java/com/example/studyit/view/NewLab.java
@@ -1,6 +1,7 @@
 //@author Баландин
 package com.example.studyit.view;
 
+import android.R.layout;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -9,28 +10,63 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.studyit.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewLab extends AppCompatActivity {
 
-    String[] items = {"1 предмет", "2 предмет", "3 предмет", "4 предмет"};
+    String[] items_discipline = {"1 предмет", "2 предмет", "3 предмет", "4 предмет"};
+    String[] items_controlPoints = {"1 КТ", "2 КТ", "3 КТ"}; //aepanov
+    private TextInputEditText textViewLabName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        textViewLabName =  findViewById(R.id.textViewLabName1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_lab);
 
-
+        //aepanov start
         Spinner spinnerSubject = (Spinner) findViewById(R.id.spinnerDiscipline);
-        Spinner spinnerControlPoint = (Spinner) findViewById(R.id.spinnerCP);
-        TextView labName = (TextView) findViewById(R.id.textViewLabName);
+        spinnerSubject.setPrompt("Дисциплина");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        Spinner spinnerControlPoint = (Spinner) findViewById(R.id.spinnerCP);
+        spinnerControlPoint.setPrompt("Контрольная точка");
+        //aepanov end
+
+        //TextView labName = (TextView) findViewById(R.id.textViewLabName);
+        textViewLabName =  findViewById(R.id.textViewLabName1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items_discipline);
         // Определяем разметку для использования при выборе элемента
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                // Применяем адаптер к элементу spinner
+        // Применяем адаптер к элементу spinner
         spinnerSubject.setAdapter(adapter);
+
+        //aepanov start
+        ArrayAdapter<String> CP_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items_controlPoints);
+        CP_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerControlPoint.setAdapter(CP_adapter);
+        //aepanov end
+
+        //maximzltrv start
+        textViewLabName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                int len = 0;
+                if (textViewLabName.getText()!=null)
+                    len = textViewLabName.getText().length();
+                if(!hasFocus && len==0){
+
+                    textViewLabName.setHint("Введите название лабораторной работы");
+                }
+                else
+                {
+                    textViewLabName.setHint("Название лабораторной работы");
+                }
+            }
+        });
+        //maximzltrv end
 
     }
 
@@ -41,6 +77,8 @@ public class NewLab extends AppCompatActivity {
         Toast toast = Toast.makeText(getApplicationContext(),
                 "ДОБААААВЬ меня", Toast.LENGTH_SHORT);
         toast.show();
+
+
 
     }
 
